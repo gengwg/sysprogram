1fe5c862f12e2e886d9ed49be6cd98b7de83b27c
Weigang Geng <gengwg@gmail.com>
2016-09-29 22:52:20 -0700

    more comments

--- chap6_getaffinity.c
+++ chap6_getaffinity.c
@@ -10,7 +10,10 @@ int main (void)
     cpu_set_t set;
     int ret, i;
 
+    /* before invocation zero out all bits in the set */
     CPU_ZERO (&set);
+    /* sched_getaffinity() retrieves the CPU affinity of the proces 
+       if pid is zero, retrieves current process's affinity. */
     ret = sched_getaffinity(0, sizeof (cpu_set_t), &set);
     if (ret == -1)
         perror ("sched_getaffinity");
@@ -24,3 +27,17 @@ int main (void)
     }
 
 }
+
+/* output 
+   $ ./a.out  | head
+   cpu=0 is set
+   cpu=1 is set
+   cpu=2 is set
+   cpu=3 is set
+   cpu=4 is unset
+   cpu=5 is unset
+   cpu=6 is unset
+   cpu=7 is unset
+   cpu=8 is unset
+   cpu=9 is unset
+ */
